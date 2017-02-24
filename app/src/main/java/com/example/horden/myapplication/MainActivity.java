c678a7d00d105c6c1026c247964d92752deb3e81
horden316 <horden316@gmail.com>
2017-02-24 22:38:57 +0800

    first commit

---
+++ app/src/main/java/com/example/horden/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.horden.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.widget.Toast;
+import android.view.View;
+
+import static com.example.horden.myapplication.R.styleable.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    void click(View view){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this , p2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/horden/myapplication/p2.java
@@ -0,0 +1,20 @@
+package com.example.horden.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class p2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p2);
+    }
+    void click2(View view){
+        Intent intent = new Intent();
+        intent.setClass(p2.this , MainActivity.class);
+        startActivity(intent);
+    }
+}

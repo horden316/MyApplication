10e890fa7ebc6b2d8580c34bada8215e1762c6b1
horden316 <horden316@gmail.com>
2017-02-25 12:10:19 +0800

    last activity toast

--- app/src/main/java/com/example/horden/myapplication/p2.java
+++ app/src/main/java/com/example/horden/myapplication/p2.java
@@ -1,9 +1,11 @@
 package com.example.horden.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class p2 extends AppCompatActivity {
 
@@ -14,6 +16,9 @@ public class p2 extends AppCompatActivity {
     }
     void click2(View view){
         this.finish();
-
+        Context context = getApplication();
+        CharSequence text = "Last activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 }
